e34073f969eb369dc170c44edfbddca364d28a3f
lorenzozane <lorzzn@qq.com>
2024-05-08 16:26:28 +0800

    update: code

--- smart_contract/contracts/MyNFT.sol
+++ smart_contract/contracts/MyNFT.sol
@@ -2,39 +2,25 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-// We inherit the contract we imported. This means we'll have access
-// to the inherited contract's methods.
 contract MyEpicNFT is ERC721 {
-    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 private _tokenIds;
 
-    // We need to pass the name of our NFTs token and its symbol.
     constructor() ERC721("SquareNFT", "SQUARE") {}
 
-    // A function our user will hit to get their NFT.
     function makeAnEpicNFT() public {
-        // Get the current tokenId, this starts at 0.
-        uint256 newItemId = _tokenIds.current();
-
-        // Actually mint the NFT to the sender using msg.sender.
+        uint256 newItemId = _tokenIds;
         _safeMint(msg.sender, newItemId);
-
-        // Return the NFT's metadata
         tokenURI(newItemId);
-
-        // Increment the counter for when the next NFT is minted.
-        _tokenIds.increment();
+        ++_tokenIds;
     }
 
-    // Set the NFT's metadata
     function tokenURI(
         uint256 _tokenId
     ) public view override returns (string memory) {
-        require(_exists(_tokenId));
+        bool ok = ownerOf(_tokenId) == msg.sender;
+        require(ok, "You are not the owner of this NFT");
         return "blah";
     }
 }
