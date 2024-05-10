c3918f2c60ec3e3dbebf3b3441516f369e8d4fa2
lorenzozane <lorzzn@qq.com>
2024-05-10 19:28:34 +0800

    update: hardhat deploy to sepolia

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,13 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     hardhat: {
       chainId: 1337,
-    }
-  }
+    },
+    sepolia: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
